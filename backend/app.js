23ae23e27af18f1abc57233952c7984129c5a483
davidosili <davidosili93@gmail.com>
2025-08-02 18:25:21 +0100

    udate on my cors for my front end

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,8 @@ app.use(cors({
     'http://localhost:5500',
     'http://127.0.0.1:5500',
     'https://mr73c5kx-5500.uks1.devtunnels.ms',
-    'https://krypt-broker-frontend.onrender.com'  // ✅ your frontend DevTunnel
+    'https://krypt-broker-frontend.onrender.com',
+     'https://krypt-broker-frontend.onrender.com' // ✅ my frontend
   ],
   credentials: true
 }));
