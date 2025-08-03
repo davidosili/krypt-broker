c530fc97dccead1ae893178718e5a21eb1259bf5
davidosili <davidosili93@gmail.com>
2025-08-03 11:02:50 +0100

    new update

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,8 @@ app.use(cors({
     'http://127.0.0.1:5500',
     'https://mr73c5kx-5500.uks1.devtunnels.ms',
      'https://krypt-broker-site.onrender.com',
-     'https://krypt-broker-frontend.onrender.com', // ✅  frontend
+     'https://krypt-broker-frontend.onrender.com',
+     'https://krypt-broker.onrender.com' // ✅  frontend
   ],
   credentials: true
 }));
